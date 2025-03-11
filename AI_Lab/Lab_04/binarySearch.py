234df1f127e0c896bca545b84ac4b7bca6569282
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2025-03-11 20:36:47 +0500

    binarySearch

--- AI_Lab/Lab_04/binarySearch.py
+++ AI_Lab/Lab_04/binarySearch.py
@@ -1,8 +1,19 @@
 def binarySearch(arr, target):
-    left , right = 0 
-    len(arr)-1
+    left , right = 0 , len(arr)-1
     
     while(left<right):
         mid = left + (right-left)//2
-        if
+        if(arr[mid]==target):
+            return mid
+        elif(arr[mid]<target):
+            left = mid+1
+        else:
+            right = mid-1
+            
+    return -1
+
+arr = [1, 3, 5, 7, 9, 11, 15]
+target = 7
+result = binarySearch(arr, target)
+print("Element found at index:", result) if result != -1 else print("Element not found")
     
